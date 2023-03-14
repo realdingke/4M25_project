Added several new worlds (no paths yet) Fixed plot.py to properly align GT/VO Added error calculation to plot.py

controllers/mapping/mapping.py
@@ -6,7 +6,7 @@ import os
 import pickle
 import numpy as np
 # import cv2 as cv
-from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
 
 TIME_STEP = 32
 
@@ -394,7 +394,7 @@ def execute(supervisor, world, fov, maxsteps=2000):
             RECORDER.update(StepCounter)
 
         #read camera image and display it
-        img = camera.getImageArray()
+        # img = camera.getImageArray()
         # img_l = cv.imread(img_l, cv.IMREAD_GRAYSCALE)
         # img_r = cv.imread(img_r, cv.IMREAD_GRAYSCALE)
         # # img_l = cv.cvtColor(img_l, cv.COLOR_BGR2GRAY)
@@ -412,9 +412,11 @@ def execute(supervisor, world, fov, maxsteps=2000):
             gt_orientation_array.append(np.array(gt_orientation).flatten())
 
             # cv.imwrite(img_dir + f"Image No.{StepCounter//20}.png", img)
-            #plt.imshow(img, interpolation='nearest')
-            plt.axis('off')
-            plt.savefig(os.path.join(img_dir, f"image-{world}-{fov}-{StepCounter//20}.png"), bbox_inches='tight', pad_inches=0)
+            # plt.imshow(img, interpolation='nearest')
+            # plt.axis('off')
+            # plt.savefig(os.path.join(img_dir, f"image-{world}-{fov}-{StepCounter//20}.png"), bbox_inches='tight', pad_inches=0)
+
+            camera.saveImage(os.path.join(img_dir, f"image-{world}-{fov}-{StepCounter//20}.png"), -1)
 
             print(f"Image No.{StepCounter//20} at step {StepCounter} saved")
 
@@ -433,8 +435,8 @@ def execute(supervisor, world, fov, maxsteps=2000):
 
 def main():
     run_all = False
-    worlds = ["world_downwardcam.wbt", "hall.wbt"]
-    fovs = [50, 120]
+    worlds = ["world_downwardcam.wbt", "hall.wbt", "city.wbt", "complete_apartment.wbt", "village_realistic.wbt"]
+    fovs = [40, 55, 70, 85, 100, 115, 130, 145, 160, 175, 190, 205, 220]
 
     os.chdir("../..")
     localpath = lambda f: os.path.join(os.getcwd()+"/worlds", f)
@@ -454,7 +456,7 @@ def main():
         pickle.dump([0, 0], open(localpath('state.p'), 'wb'))
         supervisor.worldLoad(localpath(worlds[0]))
 
-    execute(supervisor, worlds[state[0]], fovs[state[1]], maxsteps=100)
+    execute(supervisor, worlds[state[0]], fovs[state[1]])#, maxsteps=100)
 
     if state[0] == len(worlds) - 1 \
         and state[1] == len(fovs) - 1:

plot.py
@@ -1,50 +1,111 @@
 import os
 import numpy as np
+from scipy import spatial
+from sklearn.linear_model import HuberRegressor
 import matplotlib.pyplot as plt
 
-FOV = 50
-CAMERA_HEIGHT = 0.12
+FOV = 150
 PLOT_STEPS = -1
 FLOOR_SIZE = (2, 2)
 CENTER = (0, 0)
 
+
+
 # Read ground truths
-gt_pos = open(f"imgs/gt_positions-{FOV}.csv").readlines()
-gt_pos = np.array(list(map(lambda l: list(map(float, l.split(','))), gt_pos)))
+def parsePosition(line):
+    vals = list(map(float, line.split(',')))
+    return [-vals[0], vals[2]]
 
-gt_pos[:, 0] *= -1
+gt_pos = open(f"imgs/gt_positions--{FOV}.csv").readlines()
+gt_pos = np.array(list(map(parsePosition, gt_pos)))
 
-gt_rot = open(f"imgs/gt_orientations-{FOV}.csv").readlines()
-gt_rot = list(map(lambda l: list(map(float, l.split(','))), gt_rot))
-gt_rot = np.array(list(map(lambda r: [r[0:3], r[3:6], r[6:9]], gt_rot)))
+def parseRotation(line):
+    vals = list(map(float, line.split(',')))
+    mat = spatial.transform.Rotation.from_matrix([vals[0:3], vals[3:6], vals[6:9]])
+    return -mat.as_euler("xyz")[1]
+
+gt_rot = open(f"imgs/gt_orientations--{FOV}.csv").readlines()
+gt_rot = np.array(list(map(parseRotation, gt_rot)))
+
+
 
 # Read VO results
-vo_pos = [gt_pos[0] / CAMERA_HEIGHT]
-vo_rot = [gt_rot[0]]
-
+vo_pos = [[0.0, 0.0]]
+vo_rot = [0.0]
 vo_match = []
 vo_inliers = []
+T_acc = np.identity(4)
 
 lines = open(f"imgs/out-{FOV}.txt").readlines()
-T_acc = np.identity(4)
-T_acc[:3, :3] = gt_rot[0]
-T_acc[:3, 3] = vo_pos[0]
 for [pose, matches, inliers] in zip(lines[::3], lines[1::3], lines[2::3]):
     pose = list(map(float, pose.split()))
 
     T = np.array([pose[0:4], pose[4:8], pose[8:12], [0.0, 0.0, 0.0, 1.0]])
     T_acc = T @ T_acc
     pos = T_acc[:3, 3]
-    rot = T_acc[:3, :3]
+    rot = spatial.transform.Rotation.from_matrix(T_acc[:3, :3])
 
-    vo_pos.append(pos)
-    vo_rot.append(rot)
+    vo_pos.append([pos[0], pos[2]])
+    vo_rot.append(rot.as_euler("xyz")[2])
     vo_match.append(float(matches))
     vo_inliers.append(float(inliers))
 
-vo_pos = np.array(vo_pos) * CAMERA_HEIGHT
+vo_pos = np.array(vo_pos)
+vo_rot = np.array(vo_rot)
 vo_match = np.array(vo_match)
 vo_inliers = np.array(vo_inliers)
+
+
+
+# Truncate longer (in case of old images in folder / partial data)
+if len(vo_pos) < len(gt_pos):
+    gt_pos = gt_pos[:len(vo_pos)]
+    gt_rot = gt_rot[:len(vo_pos)]
+
+if len(gt_pos) < len(vo_pos):
+    vo_pos = vo_pos[:len(gt_pos)]
+    vo_rot = vo_rot[:len(gt_pos)]
+
+
+
+# Align with ground truth
+theta = gt_rot[0]
+vo_rot += theta
+theta -= np.pi / 2
+T = np.array([
+    [np.cos(theta), -np.sin(theta)],
+    [np.sin(theta), np.cos(theta)]
+])
+vo_pos = (T @ vo_pos.T).T
+
+
+
+# Calculate dposes
+gt_dpos = gt_pos[:-1] - gt_pos[1:]
+gt_drot = gt_rot[:-1] - gt_rot[1:]
+vo_dpos = vo_pos[:-1] - vo_pos[1:]
+vo_drot = vo_rot[:-1] - vo_rot[1:]
+
+
+
+# Solve for scale
+huber = HuberRegressor().fit(vo_dpos.reshape((-1, 1)), gt_dpos.ravel())
+scale = huber.coef_
+vo_dpos *= scale
+vo_pos = (vo_pos * scale) + gt_pos[0]
+
+
+
+# Compute error [Are we ready for Autonomous Driving? The KITTI Vision Benchmark Suite]
+err_rot = np.atan2(np.sin(vo_drot-gt_drot), np.cos(vo_drot-gt_drot)) # Angular differences mapped to [-pi, pi]
+E_rot = np.mean(err_rot)
+err_trans = vo_dpos - gt_dpos
+err_trans = np.sqrt(err_trans[:,0]**2 + err_trans[:,1]**2)
+E_trans = np.mean(err_trans)
+
+# ------
+# Output
+# ------
 
 x = vo_match
 print("Match:")
@@ -65,8 +126,8 @@ plt.plot((CENTER[0] - FLOOR_SIZE[0]/2, CENTER[0] + FLOOR_SIZE[0]/2), (CENTER[1] 
 plt.plot((CENTER[0] - FLOOR_SIZE[0]/2, CENTER[0] + FLOOR_SIZE[0]/2), (CENTER[1] - FLOOR_SIZE[1]/2, CENTER[1] - FLOOR_SIZE[1]/2), c='k')
 plt.plot((CENTER[0] - FLOOR_SIZE[0]/2, CENTER[0] - FLOOR_SIZE[0]/2), (CENTER[1] - FLOOR_SIZE[1]/2, CENTER[1] + FLOOR_SIZE[1]/2), c='k')
 plt.plot((CENTER[0] + FLOOR_SIZE[0]/2, CENTER[0] + FLOOR_SIZE[0]/2), (CENTER[1] - FLOOR_SIZE[1]/2, CENTER[1] + FLOOR_SIZE[1]/2), c='k')
-plt.plot(gt_pos[:PLOT_STEPS,0], gt_pos[:PLOT_STEPS,2], c='g', label="Ground Truth")
-plt.plot(vo_pos[:PLOT_STEPS,0], vo_pos[:PLOT_STEPS,2], c='b', label="Visual Odometry")
+plt.plot(gt_pos[:PLOT_STEPS,0], gt_pos[:PLOT_STEPS,1], c='g', label="Ground Truth")
+plt.plot(vo_pos[:PLOT_STEPS,0], vo_pos[:PLOT_STEPS,1], c='b', label="Visual Odometry")
 plt.axis('square')
 plt.xlim(-(FLOOR_SIZE[0]/2 + 0.5), FLOOR_SIZE[0]/2 + 0.5)
 plt.ylim(-(FLOOR_SIZE[1]/2 + 0.5), FLOOR_SIZE[1]/2 + 0.5)